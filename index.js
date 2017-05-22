32f9dd8c7a40ad921391c1e2c5e0ea37b4731de5
Ilian Iliev <ilian6806@gmail.com>
2017-05-22 22:30:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 new CodeView({
     selector:"#test",
     // noWrap: false,
-    url: 'http://ilian-iliev.herokuapp.com/resources/tests/code-view-test-data.js'
     //url: '/code-view-test-data.js'
 })
-//.writeFileContent('/code-view-test-data.js')
+.writeFileContent('../code-view-test-data.js')
 .write(['var a = true;','//33 var', "a = 'w'"]).write('a=123')
 .write("a = 'w'");
